94bba5ace43f4592f2df30ae7ee1713d5e7b01b0
palaych <palaychm@yandex.ru>
2020-02-08 15:00:13 +0000

    beryllium: Use polaris oreo fingerprint
    New fingerprint failed cts, now it check yet data security patch.
    But old oreo prop pass normal without change security data patch.
    
    Change-Id: Ia96f3f1f3cbda34a7e8ed5d8e84ebbe426f664ec

--- arrow_beryllium.mk
+++ arrow_beryllium.mk
@@ -16,7 +16,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V11.0.4.0.QEJMIXM:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/polaris/polaris:8.0.0/OPR1.170623.032/V9.5.19.0.ODGMIFA:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V11.0.4.0.QEJMIXM release-keys" \
