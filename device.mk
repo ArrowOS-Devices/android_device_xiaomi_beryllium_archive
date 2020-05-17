092431712eed03ccb8d807013ecb7621364e37f6
Bruno Martins <bgcngm@gmail.com>
2020-05-17 11:53:24 +0100

    beryllium: Switch to OSS fingerprint HIDL
    Change-Id: I2abf0e136b31c64f827bac99f391d86a8c9e1fd9

--- device.mk
+++ device.mk
@@ -59,6 +59,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm845
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
