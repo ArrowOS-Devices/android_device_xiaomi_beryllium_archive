2e05cf8d89bfb6dbca937911aa08872ccc75a1e7
ganeshi4u <ganeshvarmai4u@gmail.com>
2020-09-19 13:53:25 +0000

    beryllium: Update build fingerprint
    - beryllium-user 10 QKQ1.190828.002 V12.0.1.0.QEJMIXM release-keys
    
    Change-Id: Ic8cd4a850a0acf97aac5a2eb193499a5a9a4938b

--- arrow_beryllium.mk
+++ arrow_beryllium.mk
@@ -17,10 +17,10 @@ PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCOPHONE F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/polaris/polaris:8.0.0/OPR1.170623.032/V9.5.19.0.ODGMIFA:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.1.0.QEJMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V11.0.8.0.QEJMIXM release-keys" \
+    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.1.0.QEJMIXM release-keys" \
     PRODUCT_NAME="beryllium" \
     PRODUCT_BRAND="Xiaomi"
 
