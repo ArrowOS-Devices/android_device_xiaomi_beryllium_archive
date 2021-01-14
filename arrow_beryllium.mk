1c4ec5628e65000c93f16ed1ec4cb456df94c998
palaych <palaychm@yandex.ru>
2021-01-14 12:26:17 +0000

    beryllium: Add new maintainer
    * main maintainer for mi845
    
    Change-Id: Ic67dce5a87b92cd91a6c26dee770f93d81319b8f

--- arrow_beryllium.mk
+++ arrow_beryllium.mk
@@ -10,7 +10,7 @@ $(call inherit-product, device/xiaomi/beryllium/device.mk)
 $(call inherit-product, vendor/arrow/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-DEVICE_MAINTAINER := Ganesh Varma, kubersharma001
+DEVICE_MAINTAINER := Ganesh Varma, kubersharma001, palaych
 PRODUCT_NAME := arrow_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := POCO
