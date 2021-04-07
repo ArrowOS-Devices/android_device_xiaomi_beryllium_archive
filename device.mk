86e14372a08eb1edfd1811cc3af684fccbeca912
kubersharma001 <kubersharma001@gmail.com>
2021-04-07 12:45:37 +0000

    beryllium: Properly set bootanimation flag
     * Remove LineageOS specific bootanim hxw flags

--- device.mk
+++ device.mk
@@ -10,10 +10,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/beryllium/beryllium-vendor.mk)
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2246
-TARGET_SCREEN_WIDTH := 1080
-
 # Camera
 PRODUCT_PACKAGES += \
     GCamGOPrebuilt
